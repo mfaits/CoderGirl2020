68dc4bad64ee568142ee354e9fceabba7b500450
Michelle Faits <michelle.faits@graybar.com>
2020-01-29 14:44:48 -0600

    start tracking repo

---
+++ function_example.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def greet(name):
+    
+    greeting = "Hello " + name + ", it's nice to meet you"
+    
+    return greeting
+
+name = 'Michelle'
+
+greeting = greet(name)
+
+print(greeting)
---
+++ function_example2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+person1 = "jamie, Lannister, knight"
+person2 = "arya, Stark, assassin"
+person3 = "yara, Greyjoy, sailor"
+person4 = "daenerys, Targaryen, dragon-tamer"
+person5 = "jon, Snow, nightswatchman"
+
+#Capitalize the first names
+#then add a string denoting these characters are from "Game of Thrones"
+#then split on ", "
